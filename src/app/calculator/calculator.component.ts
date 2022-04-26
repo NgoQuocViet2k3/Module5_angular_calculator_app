63d0dd79035972c9ce0663eeae43069412620f59
ruukichi <ngovietlc93@gmail.com>
2022-04-26 14:21:40 +0700

    ung dung may tinh don gian

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  firstOperand = 0;
+  secondOperand = 0;
+  result = 0;
+  operand = 'ADD';
+
+  caculate() {
+    switch (this.operand) {
+      case 'ADD':
+        this.result = this.firstOperand + this.secondOperand;
+        break;
+      case'SUBTRACT':
+        this.result = this.firstOperand - this.secondOperand;
+        break;
+      case 'MULTIPLY':
+        this.result = this.firstOperand * this.secondOperand;
+        break;
+      case 'DIVIDE':
+        this.result = this.firstOperand / this.secondOperand;
+        break;
+    }
+  }
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
